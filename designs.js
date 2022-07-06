62892b4c390ece5d4822c4bbfacda07062bd2365
Destiny Ross <destinylaross@gmail.com>
2022-07-06 01:00:54 -0400

    Comments. Updated to include comments and small js refactor to address relevant spec recommendations

--- designs.js
+++ designs.js
@@ -1,33 +1,32 @@
 let color = document.getElementById("colorPicker");
 let table = document.getElementById("pixelCanvas");
 let form = document.getElementById("sizePicker");
-
-let rows = document.getElementById("inputRows").value;
-let columns = document.getElementById("inputColumns").value;
-let submitButton = document.querySelector('input[type="submit"]');
+let submitButton = document.getElementById("submit");
 
 let draw = false;
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
+// draws the grid
+function makeGrid() {
   let rows = document.getElementById("inputRows").value;
   let columns = document.getElementById("inputColumns").value;
-  table.firstElementChild.remove();
 
-  makeGrid(rows, columns);
-});
-
-function makeGrid(rows, columns) {
+  // clear old grid
   table.innerHTML = "";
 
-  for (let i = 0; i < rows; i++) {
-    let row = table.insertRow(i);
-    for (let j = 0; j < columns; j++) {
-      let cell = row.insertCell(j);
+  for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
+    // insert table row via built in table.insertRow function,
+    // then iterate through number of desired columns to generate cells
+    // https://www.w3schools.com/jsref/met_table_insertrow.asp
+    let row = table.insertRow(rowIndex);
+    for (let cellIndex = 0; cellIndex < columns; cellIndex++) {
+      let cell = row.insertCell(cellIndex);
+
+      // adds a listener on mousedown event to color cells when mouse is depressed
       cell.addEventListener("mousedown", () => {
         cell.style.backgroundColor = color.value;
       });
+      // adds a listener on mouseover event to color cells on
+      // mouse over if draw state is true
       cell.addEventListener("mouseover", () => {
         if (!draw) {
           return;
@@ -38,18 +37,19 @@ function makeGrid(rows, columns) {
   }
 }
 
-submitButton.addEventListener("click", () => {
-  makeGrid(rows, columns);
+// adds the click event to the submit button
+submitButton.addEventListener("click", (event) => {
+  //prevent default to prevent browser from sending http request to the server
+  event.preventDefault();
+  makeGrid();
 });
 
+// on mouse down, draw state should be true
 window.addEventListener("mousedown", () => {
   draw = true;
 });
 
+// on mouse up, revert draw state to false
 window.addEventListener("mouseup", () => {
   draw = false;
-});
-
-window.addEventListener("load", (event) => {
-  makeGrid(rows, columns);
 });
